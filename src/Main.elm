add6380c15d8b9053db82c60248a9448b1a5df46
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-14 16:32:44 +0530

    extracting Tree and Zipper

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,12 @@ port module Main exposing (main)
 import Browser
 import Browser.Events
 import Forest.Tree as Tree exposing (Forest, Tree)
+import Forest.Zipper as Zipper exposing (ForestZipper)
 import Html exposing (Attribute, Html, div, input, text)
 import Html.Attributes exposing (attribute, class, draggable, style, value)
 import Html.Events as Event exposing (onClick, onInput)
 import Json.Decode as JD exposing (Decoder)
 import Json.Encode as JE exposing (Value)
-import Maybe.Extra
 import Random exposing (Generator)
 import Random.Extra
 
@@ -96,7 +96,7 @@ itemEncoder item =
         ]
 
 
-crumbEncoder : Crumb Item -> Value
+crumbEncoder : Zipper.Crumb Item -> Value
 crumbEncoder crumb =
     JE.object
         [ ( "leftReversed", JE.list itemTreeEncoder crumb.leftReversed )
@@ -132,9 +132,9 @@ itemDecoder =
         |> required "title" JD.string
 
 
-crumbDecoder : Decoder (Crumb Item)
+crumbDecoder : Decoder (Zipper.Crumb Item)
 crumbDecoder =
-    JD.succeed Crumb
+    JD.succeed Zipper.Crumb
         |> required "leftReversed" (JD.list treeDecoder)
         |> required "datum" itemDecoder
         |> required "right_" (JD.list treeDecoder)
@@ -206,7 +206,7 @@ init flags =
                 Ok got ->
                     case got of
                         Nothing ->
-                            fromForest outline
+                            Zipper.fromForest outline
 
                         Just oz_ ->
                             Just oz_
@@ -492,6 +492,10 @@ update message model =
                     Debug.todo "impl"
 
 
+withRollback func oz =
+    func oz |> Maybe.withDefault oz
+
+
 ozTitle : OZ -> String
 ozTitle =
     ozItem >> .title
@@ -499,7 +503,7 @@ ozTitle =
 
 ozItem : OZ -> Item
 ozItem =
-    getTree >> Tree.data
+    Zipper.data
 
 
 ozId : OZ -> ItemId
@@ -509,12 +513,12 @@ ozId =
 
 ozSetTitle : String -> OZ -> OZ
 ozSetTitle title =
-    fzMapData (\item -> { item | title = title })
+    Zipper.mapData (\item -> { item | title = title })
 
 
 gotoItemId : ItemId -> OZ -> Maybe OZ
 gotoItemId itemId =
-    findFirst (propEq .id itemId)
+    Zipper.findFirst (propEq .id itemId)
 
 
 moveItemWithIdToCandidateLocation : ItemId -> CandidateLocation -> OZ -> Maybe OZ
@@ -522,7 +526,7 @@ moveItemWithIdToCandidateLocation srcItemId candidateLocation =
     let
         moveTo : CandidateLocation -> OZ -> Maybe OZ
         moveTo atLocation zipper =
-            remove zipper
+            Zipper.remove zipper
                 |> Maybe.andThen (insertRemovedNodeAtLocation atLocation zipper.center)
 
         insertRemovedNodeAtLocation : CandidateLocation -> OutlineNode -> OZ -> Maybe OZ
@@ -534,16 +538,16 @@ moveItemWithIdToCandidateLocation srcItemId candidateLocation =
             in
             case atLocation of
                 Before itemId ->
-                    insertHelp itemId insertLeft
+                    insertHelp itemId Zipper.insertLeft
 
                 After itemId ->
-                    insertHelp itemId insertAndGoRight
+                    insertHelp itemId Zipper.insertAndGoRight
 
                 PrependIn itemId ->
-                    insertHelp itemId prependAndGotoChild
+                    insertHelp itemId Zipper.prependAndGotoChild
 
                 AppendIn itemId ->
-                    insertHelp itemId insertLastChild
+                    insertHelp itemId Zipper.insertLastChild
     in
     gotoItemId srcItemId
         >> Maybe.andThen (moveTo candidateLocation)
@@ -619,7 +623,7 @@ viewExpOutline outline =
                     in
                     transformForest
                         (\item -> renderWithBeacons (item.id == highlightedId) item)
-                        (toRootForest oz)
+                        (Zipper.toRootForest oz)
 
                 OutlineDnD dnd oz ->
                     transformForestWithContext
@@ -639,7 +643,7 @@ viewExpOutline outline =
                                     ctx
                         }
                         { renderWithoutBeacons = False }
-                        (toRootForest oz)
+                        (Zipper.toRootForest oz)
 
                 OutlineEdit oz title ->
                     transformForest
@@ -650,7 +654,7 @@ viewExpOutline outline =
                             else
                                 renderWithBeacons False item
                         )
-                        (toRootForest oz)
+                        (Zipper.toRootForest oz)
     in
     div []
         [ div [ class "f1" ] [ text "exp tree view" ]
@@ -674,7 +678,7 @@ viewExpDraggedNode outline =
 
                 draggedForest =
                     gotoItemId dnd.dragItemId oz
-                        |> Maybe.map (getTree >> List.singleton)
+                        |> Maybe.map (Zipper.getTree >> List.singleton)
                         |> Maybe.withDefault []
 
                 draggedHtmlList =
@@ -1070,364 +1074,6 @@ dragEvents itemId =
 preventDefault : Bool -> Decoder b -> Decoder ( b, Bool )
 preventDefault bool =
     JD.map (\msg -> ( msg, bool ))
-
-
-
--- FOREST ZIPPER
-
-
-type alias ForestZipper a =
-    { leftReversed : Forest a
-    , center : Tree a
-    , right_ : Forest a
-    , crumbs : List (Crumb a)
-    }
-
-
-type alias Crumb a =
-    { leftReversed : Forest a
-    , datum : a
-    , right_ : Forest a
-    }
-
-
-
---noinspection ElmUnusedSymbol
-
-
-fromSingletonForest : Tree a -> ForestZipper a
-fromSingletonForest tree =
-    { leftReversed = [], center = tree, right_ = [], crumbs = [] }
-
-
-fromForest : Forest a -> Maybe (ForestZipper a)
-fromForest forest =
-    case forest of
-        [] ->
-            Nothing
-
-        first :: rest ->
-            Just { leftReversed = [], center = first, right_ = rest, crumbs = [] }
-
-
-toRootForest : ForestZipper a -> Forest a
-toRootForest =
-    firstRoot >> getForest
-
-
-getForest : ForestZipper a -> Forest a
-getForest fz =
-    List.reverse fz.leftReversed ++ fz.center :: fz.right_
-
-
-withRollback : (ForestZipper a -> Maybe (ForestZipper a)) -> ForestZipper a -> ForestZipper a
-withRollback func oz =
-    func oz |> Maybe.withDefault oz
-
-
-mapTree : (Tree a -> Tree a) -> ForestZipper a -> ForestZipper a
-mapTree func fz =
-    { fz | center = func fz.center }
-
-
-fzMapData : (a -> a) -> ForestZipper a -> ForestZipper a
-fzMapData func =
-    mapTree (Tree.mapData func)
-
-
-getTree : ForestZipper a -> Tree a
-getTree fz =
-    fz.center
-
-
-
---getLevel : ForestZipper a -> Int
---getLevel fz =
---    List.length fz.crumbs
---noinspection ElmUnusedSymbol
-
-
-isFirst : ForestZipper a -> Bool
-isFirst fz =
-    List.isEmpty fz.leftReversed
-
-
-
---noinspection ElmUnusedSymbol
-
-
-isLast : ForestZipper a -> Bool
-isLast fz =
-    List.isEmpty fz.right_
-
-
-
--- CORE NAVIGATION
-
-
-{-|
-
-    @docs up, down, left, right.
-    alternative names:
-        ascend, descend, before, after, respectively.
-
-    Alternatives names for: left/right:
-        before/after; previousSibling/nextSibling.
-
--}
-up : ForestZipper a -> Maybe (ForestZipper a)
-up acc =
-    case acc.crumbs of
-        [] ->
-            Nothing
-
-        { leftReversed, datum, right_ } :: rest ->
-            Just
-                { acc
-                    | leftReversed = leftReversed
-                    , center = Tree.tree datum (List.reverse acc.leftReversed ++ acc.center :: acc.right_)
-                    , right_ = right_
-                    , crumbs = rest
-                }
-
-
-down : ForestZipper a -> Maybe (ForestZipper a)
-down acc =
-    case Tree.toTuple acc.center of
-        ( _, [] ) ->
-            Nothing
-
-        ( a, first :: rest ) ->
-            Just
-                { acc
-                    | leftReversed = []
-                    , center = first
-                    , right_ = rest
-                    , crumbs =
-                        { leftReversed = acc.leftReversed, datum = a, right_ = acc.right_ }
-                            :: acc.crumbs
-                }
-
-
-left : ForestZipper a -> Maybe (ForestZipper a)
-left acc =
-    case acc.leftReversed of
-        [] ->
-            Nothing
-
-        first :: rest ->
-            Just
-                { acc
-                    | leftReversed = rest
-                    , center = first
-                    , right_ = acc.center :: acc.right_
-                }
-
-
-right : ForestZipper a -> Maybe (ForestZipper a)
-right acc =
-    case acc.right_ of
-        [] ->
-            Nothing
-
-        first :: rest ->
-            Just
-                { acc
-                    | leftReversed = acc.center :: acc.leftReversed
-                    , center = first
-                    , right_ = rest
-                }
-
-
-
--- NAVIGATION HELPER
-
-
-applyWhileJust : (a -> Maybe a) -> a -> a
-applyWhileJust func a =
-    case func a of
-        Just a2 ->
-            applyWhileJust func a2
-
-        Nothing ->
-            a
-
-
-root : ForestZipper a -> ForestZipper a
-root =
-    applyWhileJust up
-
-
-firstRoot : ForestZipper a -> ForestZipper a
-firstRoot =
-    root >> applyWhileJust left
-
-
-nextSiblingOfClosestAncestor : ForestZipper a -> Maybe (ForestZipper a)
-nextSiblingOfClosestAncestor acc =
-    case up acc of
-        Just parentAcc ->
-            case right parentAcc of
-                Just ns ->
-                    Just ns
-
-                Nothing ->
-                    nextSiblingOfClosestAncestor parentAcc
-
-        Nothing ->
-            Nothing
-
-
-findFromCurrent : (a -> Bool) -> ForestZipper a -> Maybe (ForestZipper a)
-findFromCurrent pred acc =
-    case Tree.data acc.center of
-        a ->
-            if pred a then
-                Just acc
-
-            else
-                case Maybe.Extra.oneOf [ down, right, nextSiblingOfClosestAncestor ] acc of
-                    Just nextAcc ->
-                        findFromCurrent pred nextAcc
-
-                    Nothing ->
-                        Nothing
-
-
-findFirst : (a -> Bool) -> ForestZipper a -> Maybe (ForestZipper a)
-findFirst pred acc =
-    firstRoot acc |> findFromCurrent pred
-
-
-
--- VISIT
---
---
---fzVisit :
---    { enter : ForestZipper a -> acc -> acc
---    , exit : ForestZipper a -> acc -> acc
---    }
---    -> acc
---    -> ForestZipper a
---    -> acc
---fzVisit { enter, exit } =
---    let
---        step : VisitMsg -> acc -> ForestZipper a -> acc
---        step msg acc oz =
---            case msg of
---                Enter ->
---                    step Entered (enter oz acc) oz
---
---                Entered ->
---                    case down oz of
---                        Just childOZ ->
---                            step Enter acc childOZ
---
---                        Nothing ->
---                            step Exit acc oz
---
---                Exit ->
---                    step Exited (exit oz acc) oz
---
---                Exited ->
---                    case right oz of
---                        Just rightOZ ->
---                            step Enter acc rightOZ
---
---                        Nothing ->
---                            step Up acc oz
---
---                Up ->
---                    case up oz of
---                        Just parentOZ ->
---                            step Exit acc parentOZ
---
---                        Nothing ->
---                            acc
---
---        enterFirstRoot : acc -> ForestZipper a -> acc
---        enterFirstRoot acc fz =
---            step Enter acc (firstRoot fz)
---    in
---    enterFirstRoot
---
---
---type VisitMsg
---    = Enter
---    | Entered
---    | Exit
---    | Exited
---    | Up
--- INSERTION
-
-
-insertLeft : Tree a -> ForestZipper a -> ForestZipper a
-insertLeft tree acc =
-    { acc | leftReversed = tree :: acc.leftReversed }
-
-
-insertAndGoRight : Tree a -> ForestZipper a -> ForestZipper a
-insertAndGoRight =
-    let
-        insertRight : Tree a -> ForestZipper a -> ForestZipper a
-        insertRight tree acc =
-            { acc | right_ = tree :: acc.right_ }
-
-        insertHelp : Tree a -> ForestZipper a -> Maybe (ForestZipper a)
-        insertHelp tree =
-            insertRight tree >> right
-    in
-    withRollback << insertHelp
-
-
-prependAndGotoChild : Tree a -> ForestZipper a -> ForestZipper a
-prependAndGotoChild child acc =
-    case Tree.toTuple acc.center of
-        ( a, children ) ->
-            { acc
-                | center = child
-                , leftReversed = []
-                , right_ = children
-                , crumbs =
-                    { leftReversed = acc.leftReversed, datum = a, right_ = acc.right_ }
-                        :: acc.crumbs
-            }
-
-
-insertLastChild : Tree a -> ForestZipper a -> ForestZipper a
-insertLastChild child acc =
-    case Tree.toTuple acc.center of
-        ( a, children ) ->
-            { acc | center = Tree.tree a (children ++ [ child ]) }
-
-
-
--- DELETION
-
-
-remove : ForestZipper a -> Maybe (ForestZipper a)
-remove acc =
-    case ( acc.leftReversed, acc.right_ ) of
-        ( [], [] ) ->
-            case acc.crumbs of
-                [] ->
-                    --Nothing
-                    Debug.todo "should not happen"
-
-                { leftReversed, datum, right_ } :: rest ->
-                    Just
-                        { acc
-                            | leftReversed = leftReversed
-                            , center = Tree.tree datum []
-                            , right_ = right_
-                            , crumbs = rest
-                        }
-
-        ( first :: rest, _ ) ->
-            Just { acc | leftReversed = rest, center = first }
-
-        ( _, first :: rest ) ->
-            Just { acc | center = first, right_ = rest }
 
 
 
--- tree-src/Forest/Zipper.elm
+++ tree-src/Forest/Zipper.elm
@@ -1,18 +1,367 @@
-module Forest.Zipper exposing (Zipper, fromForest)
+module Forest.Zipper exposing (Crumb, ForestZipper, data, findFirst, fromForest, getTree, insertAndGoRight, insertLastChild, insertLeft, mapData, prependAndGotoChild, remove, toRootForest)
 
-import Forest exposing (Forest)
-import Tree.Zipper exposing (Zipper)
-
-
-type alias Zipper a =
-    Tree.Zipper.Zipper a
+import Forest.Tree as Tree exposing (Forest, Tree)
+import Maybe.Extra
 
 
-fromForest : Forest a -> Maybe (Zipper a)
+
+-- FOREST ZIPPER
+
+
+type alias ForestZipper a =
+    { leftReversed : Forest a
+    , center : Tree a
+    , right_ : Forest a
+    , crumbs : List (Crumb a)
+    }
+
+
+type alias Crumb a =
+    { leftReversed : Forest a
+    , datum : a
+    , right_ : Forest a
+    }
+
+
+
+--noinspection ElmUnusedSymbol
+
+
+fromSingletonForest : Tree a -> ForestZipper a
+fromSingletonForest tree =
+    { leftReversed = [], center = tree, right_ = [], crumbs = [] }
+
+
+fromForest : Forest a -> Maybe (ForestZipper a)
 fromForest forest =
     case forest of
-        first :: rest ->
-            Just (Tree.Zipper.fromForest first rest)
-
         [] ->
             Nothing
+
+        first :: rest ->
+            Just { leftReversed = [], center = first, right_ = rest, crumbs = [] }
+
+
+toRootForest : ForestZipper a -> Forest a
+toRootForest =
+    firstRoot >> getForest
+
+
+getForest : ForestZipper a -> Forest a
+getForest fz =
+    List.reverse fz.leftReversed ++ fz.center :: fz.right_
+
+
+withRollback : (ForestZipper a -> Maybe (ForestZipper a)) -> ForestZipper a -> ForestZipper a
+withRollback func oz =
+    func oz |> Maybe.withDefault oz
+
+
+mapTree : (Tree a -> Tree a) -> ForestZipper a -> ForestZipper a
+mapTree func fz =
+    { fz | center = func fz.center }
+
+
+mapData : (a -> a) -> ForestZipper a -> ForestZipper a
+mapData func =
+    mapTree (Tree.mapData func)
+
+
+getTree : ForestZipper a -> Tree a
+getTree fz =
+    fz.center
+
+
+data : ForestZipper a -> a
+data =
+    getTree >> Tree.data
+
+
+
+--getLevel : ForestZipper a -> Int
+--getLevel fz =
+--    List.length fz.crumbs
+--noinspection ElmUnusedSymbol
+
+
+isFirst : ForestZipper a -> Bool
+isFirst fz =
+    List.isEmpty fz.leftReversed
+
+
+
+--noinspection ElmUnusedSymbol
+
+
+isLast : ForestZipper a -> Bool
+isLast fz =
+    List.isEmpty fz.right_
+
+
+
+-- CORE NAVIGATION
+
+
+{-|
+
+    @docs up, down, left, right.
+    alternative names:
+        ascend, descend, before, after, respectively.
+
+    Alternatives names for: left/right:
+        before/after; previousSibling/nextSibling.
+
+-}
+up : ForestZipper a -> Maybe (ForestZipper a)
+up acc =
+    case acc.crumbs of
+        [] ->
+            Nothing
+
+        { leftReversed, datum, right_ } :: rest ->
+            Just
+                { acc
+                    | leftReversed = leftReversed
+                    , center = Tree.tree datum (List.reverse acc.leftReversed ++ acc.center :: acc.right_)
+                    , right_ = right_
+                    , crumbs = rest
+                }
+
+
+down : ForestZipper a -> Maybe (ForestZipper a)
+down acc =
+    case Tree.toTuple acc.center of
+        ( _, [] ) ->
+            Nothing
+
+        ( a, first :: rest ) ->
+            Just
+                { acc
+                    | leftReversed = []
+                    , center = first
+                    , right_ = rest
+                    , crumbs =
+                        { leftReversed = acc.leftReversed, datum = a, right_ = acc.right_ }
+                            :: acc.crumbs
+                }
+
+
+left : ForestZipper a -> Maybe (ForestZipper a)
+left acc =
+    case acc.leftReversed of
+        [] ->
+            Nothing
+
+        first :: rest ->
+            Just
+                { acc
+                    | leftReversed = rest
+                    , center = first
+                    , right_ = acc.center :: acc.right_
+                }
+
+
+right : ForestZipper a -> Maybe (ForestZipper a)
+right acc =
+    case acc.right_ of
+        [] ->
+            Nothing
+
+        first :: rest ->
+            Just
+                { acc
+                    | leftReversed = acc.center :: acc.leftReversed
+                    , center = first
+                    , right_ = rest
+                }
+
+
+
+-- NAVIGATION HELPER
+
+
+applyWhileJust : (a -> Maybe a) -> a -> a
+applyWhileJust func a =
+    case func a of
+        Just a2 ->
+            applyWhileJust func a2
+
+        Nothing ->
+            a
+
+
+root : ForestZipper a -> ForestZipper a
+root =
+    applyWhileJust up
+
+
+firstRoot : ForestZipper a -> ForestZipper a
+firstRoot =
+    root >> applyWhileJust left
+
+
+nextSiblingOfClosestAncestor : ForestZipper a -> Maybe (ForestZipper a)
+nextSiblingOfClosestAncestor acc =
+    case up acc of
+        Just parentAcc ->
+            case right parentAcc of
+                Just ns ->
+                    Just ns
+
+                Nothing ->
+                    nextSiblingOfClosestAncestor parentAcc
+
+        Nothing ->
+            Nothing
+
+
+findFromCurrent : (a -> Bool) -> ForestZipper a -> Maybe (ForestZipper a)
+findFromCurrent pred acc =
+    case Tree.data acc.center of
+        a ->
+            if pred a then
+                Just acc
+
+            else
+                case Maybe.Extra.oneOf [ down, right, nextSiblingOfClosestAncestor ] acc of
+                    Just nextAcc ->
+                        findFromCurrent pred nextAcc
+
+                    Nothing ->
+                        Nothing
+
+
+findFirst : (a -> Bool) -> ForestZipper a -> Maybe (ForestZipper a)
+findFirst pred acc =
+    firstRoot acc |> findFromCurrent pred
+
+
+
+-- VISIT
+--
+--
+--fzVisit :
+--    { enter : ForestZipper a -> acc -> acc
+--    , exit : ForestZipper a -> acc -> acc
+--    }
+--    -> acc
+--    -> ForestZipper a
+--    -> acc
+--fzVisit { enter, exit } =
+--    let
+--        step : VisitMsg -> acc -> ForestZipper a -> acc
+--        step msg acc oz =
+--            case msg of
+--                Enter ->
+--                    step Entered (enter oz acc) oz
+--
+--                Entered ->
+--                    case down oz of
+--                        Just childOZ ->
+--                            step Enter acc childOZ
+--
+--                        Nothing ->
+--                            step Exit acc oz
+--
+--                Exit ->
+--                    step Exited (exit oz acc) oz
+--
+--                Exited ->
+--                    case right oz of
+--                        Just rightOZ ->
+--                            step Enter acc rightOZ
+--
+--                        Nothing ->
+--                            step Up acc oz
+--
+--                Up ->
+--                    case up oz of
+--                        Just parentOZ ->
+--                            step Exit acc parentOZ
+--
+--                        Nothing ->
+--                            acc
+--
+--        enterFirstRoot : acc -> ForestZipper a -> acc
+--        enterFirstRoot acc fz =
+--            step Enter acc (firstRoot fz)
+--    in
+--    enterFirstRoot
+--
+--
+--type VisitMsg
+--    = Enter
+--    | Entered
+--    | Exit
+--    | Exited
+--    | Up
+-- INSERTION
+
+
+insertLeft : Tree a -> ForestZipper a -> ForestZipper a
+insertLeft tree acc =
+    { acc | leftReversed = tree :: acc.leftReversed }
+
+
+insertAndGoRight : Tree a -> ForestZipper a -> ForestZipper a
+insertAndGoRight =
+    let
+        insertRight : Tree a -> ForestZipper a -> ForestZipper a
+        insertRight tree acc =
+            { acc | right_ = tree :: acc.right_ }
+
+        insertHelp : Tree a -> ForestZipper a -> Maybe (ForestZipper a)
+        insertHelp tree =
+            insertRight tree >> right
+    in
+    withRollback << insertHelp
+
+
+prependAndGotoChild : Tree a -> ForestZipper a -> ForestZipper a
+prependAndGotoChild child acc =
+    case Tree.toTuple acc.center of
+        ( a, children ) ->
+            { acc
+                | center = child
+                , leftReversed = []
+                , right_ = children
+                , crumbs =
+                    { leftReversed = acc.leftReversed, datum = a, right_ = acc.right_ }
+                        :: acc.crumbs
+            }
+
+
+insertLastChild : Tree a -> ForestZipper a -> ForestZipper a
+insertLastChild child acc =
+    case Tree.toTuple acc.center of
+        ( a, children ) ->
+            { acc | center = Tree.tree a (children ++ [ child ]) }
+
+
+
+-- DELETION
+
+
+remove : ForestZipper a -> Maybe (ForestZipper a)
+remove acc =
+    case ( acc.leftReversed, acc.right_ ) of
+        ( [], [] ) ->
+            case acc.crumbs of
+                [] ->
+                    --Nothing
+                    Debug.todo "should not happen"
+
+                { leftReversed, datum, right_ } :: rest ->
+                    Just
+                        { acc
+                            | leftReversed = leftReversed
+                            , center = Tree.tree datum []
+                            , right_ = right_
+                            , crumbs = rest
+                        }
+
+        ( first :: rest, _ ) ->
+            Just { acc | leftReversed = rest, center = first }
+
+        ( _, first :: rest ) ->
+            Just { acc | center = first, right_ = rest }
