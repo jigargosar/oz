7e5240814b5014de25d736c3a25ee3528133c569
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-18 19:51:34 +0530

    outline model refactoring

--- src/Main.elm
+++ src/Main.elm
@@ -605,24 +605,6 @@ targetInputOrButton ke =
     List.member ke.targetTagName [ "INPUT", "BUTTON" ]
 
 
-required : String -> Decoder a -> Decoder (a -> b) -> Decoder b
-required name decoder_ =
-    JD.map2 (|>) (JD.field name decoder_)
-
-
-requiredAt : List String -> Decoder a -> Decoder (a -> b) -> Decoder b
-requiredAt path decoder_ =
-    JD.map2 (|>) (JD.at path decoder_)
-
-
-requiredBool name =
-    required name JD.bool
-
-
-requiredString name =
-    required name JD.string
-
-
 
 -- View
 
--- src/Utils.elm
+++ src/Utils.elm
@@ -1,5 +1,7 @@
 module Utils exposing (..)
 
+import Json.Decode as JD exposing (Decoder)
+
 
 eqBy func a b =
     func a == func b
@@ -7,3 +9,21 @@ eqBy func a b =
 
 neqBy func a b =
     func a /= func b
+
+
+required : String -> Decoder a -> Decoder (a -> b) -> Decoder b
+required name decoder_ =
+    JD.map2 (|>) (JD.field name decoder_)
+
+
+requiredAt : List String -> Decoder a -> Decoder (a -> b) -> Decoder b
+requiredAt path decoder_ =
+    JD.map2 (|>) (JD.at path decoder_)
+
+
+requiredBool name =
+    required name JD.bool
+
+
+requiredString name =
+    required name JD.string
