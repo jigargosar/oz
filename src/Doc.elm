216432168e42cdb4f349f64f2f1b2af4575f8a8b
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-20 17:13:35 +0530

    trying out dict based doc

--- src/Doc.elm
+++ src/Doc.elm
@@ -1,12 +1,12 @@
-module Doc exposing (Doc)
+module Doc exposing (Doc, new)
 
 import Dict exposing (Dict)
 import ItemId exposing (ItemId)
 import Random exposing (Generator)
 
 
-type Doc
-    = Doc Model
+
+-- LINE
 
 
 type alias Line =
@@ -15,13 +15,55 @@ type alias Line =
     }
 
 
+stringId : Line -> String
+stringId =
+    .id >> ItemId.toString
+
+
 newLine : Generator Line
 newLine =
     ItemId.generator
         |> Random.map (\id -> Line id "")
 
 
+
+-- MODEL
+
+
+type Doc
+    = Doc Model
+
+
 type alias Model =
     { byId : Dict String Line
     , focusedId : ItemId
     }
+
+
+type alias LineD =
+    Dict String Line
+
+
+insertNew : Line -> LineD -> LineD
+insertNew line =
+    ensureNotMember line >> Dict.insert (stringId line) line
+
+
+ensureNotMember : Line -> LineD -> LineD
+ensureNotMember line lineD =
+    if Dict.member (stringId line) lineD then
+        Debug.todo "new line already exists"
+
+    else
+        lineD
+
+
+new : Generator Doc
+new =
+    let
+        newHelp : Line -> Model
+        newHelp line =
+            { byId = insertNew line Dict.empty, focusedId = line.id }
+    in
+    newLine
+        |> Random.map (newHelp >> Doc)
