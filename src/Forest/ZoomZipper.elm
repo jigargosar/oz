ddda8e2e1f2d59c60b1ca37f9a9d18c00d89244d
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-23 11:55:25 +0530

    experimenting with LineZipper.elm

---
+++ src/Forest/ZoomZipper.elm
@@ -0,0 +1,75 @@
+module Forest.Zipper exposing (ZoomZipper, fromData, insertRightGo, right)
+
+import Forest.Tree as T exposing (Forest, Tree)
+import Json.Decode as JD exposing (Decoder)
+import Json.Encode as JE exposing (Value)
+import Utils exposing (..)
+
+
+
+-- FOREST ZIPPER MODEL
+
+
+type ZoomZipper a
+    = ZoomZipper (List (Crumb a)) (List (Crumb a)) (Forest a) (Tree a) (Forest a)
+
+
+type Crumb a
+    = Crumb (Forest a) a (Forest a)
+
+
+fromData : a -> ZoomZipper a
+fromData a =
+    ZoomZipper [] [] [] (T.singleton a) []
+
+
+
+-- Nav
+
+
+right : ZoomZipper a -> Maybe (ZoomZipper a)
+right (ZoomZipper pcs cs lfr c rf) =
+    case rf of
+        [] ->
+            Nothing
+
+        first :: rest ->
+            Just
+                (ZoomZipper pcs cs (c :: lfr) first rest)
+
+
+insertRightGo : Tree a -> ZoomZipper a -> ZoomZipper a
+insertRightGo node (ZoomZipper pcs cs lfr c rf) =
+    ZoomZipper pcs cs (c :: lfr) node rf
+
+
+insertLeftGo : Tree a -> ZoomZipper a -> ZoomZipper a
+insertLeftGo node (ZoomZipper pcs cs lfr c rf) =
+    ZoomZipper pcs cs lfr node (c :: rf)
+
+
+appendChildGo : Tree a -> ZoomZipper a -> ZoomZipper a
+appendChildGo node (ZoomZipper pcs cs lfr c rf) =
+    ZoomZipper pcs
+        (Crumb lfr (T.data c) rf :: cs)
+        (T.children c |> List.reverse)
+        node
+        []
+
+
+
+--prependChildGo : Tree a -> ZoomZipper a -> ZoomZipper a
+--prependChildGo =
+--    insertAndGo prependChild down
+
+
+insertAndGo :
+    (Tree a -> ZoomZipper a -> ZoomZipper a)
+    -> (ZoomZipper a -> Maybe (ZoomZipper a))
+    -> Tree a
+    -> ZoomZipper a
+    -> ZoomZipper a
+insertAndGo insertFunc focusFunc node zipper =
+    insertFunc node zipper
+        |> focusFunc
+        |> Maybe.withDefault zipper
